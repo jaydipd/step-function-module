ff86b2042d7f08bc8643cbde5b7e6820acb2b063
jaydipd <jay.android14@gmail.com>
2025-06-06 14:32:46 +0530

    step function module

---
+++ variable.tf
@@ -0,0 +1,18 @@
+variable "definition"{
+    description = "The JSON definition of the Step function state machine"
+    type = string
+}
+variable "name" {
+  description = "Name of the Step Function"
+  type        = string
+}
+
+variable "role_arn" {
+  description = "IAM Role ARN to be assumed by Step Function"
+  type        = string
+}
+
+variable "lambda_arn" {
+  description = "ARN of the Lambda function to invoke"
+  type        = string
+}
